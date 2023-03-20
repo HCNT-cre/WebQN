8c8443d637a60c2bae95bcd7410426ae49da8dfe
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-03-20 22:26:49 +0700

    add side bar and header

--- src/components/Layout/Sidebar/index.js
+++ src/components/Layout/Sidebar/index.js
@@ -1,6 +1,43 @@
-const SideBar = () =>{
-    return(
+const SideBar = ({sideBarWidth}) => {
+    console.log(sideBarWidth === 200);
+    return (
         <>
+            <div className={`${sideBarWidth === 200 ? "w-[200px]" : "w-[50px]"} fixed left-0 top-0 pt-[60px] shadow h-full my-[4px]`}>
+                <ul>
+                    <li className="sidebar-items">
+                        <div className="mr-[8px]"><i class="fa-solid fa-house"></i></div>
+                        <span>Hồ sơ tài liệu</span>
+                    </li>
+                    <li className="sidebar-items">
+                        <div className="mr-[8px]"><i class="fa-solid fa-house"></i></div>
+                        <span>Hồ sơ tài liệu</span>
+                    </li>
+                    <li className="sidebar-items">
+                        <div className="mr-[8px]"><i class="fa-solid fa-house"></i></div>
+                        <span>Hồ sơ tài liệu</span>
+                    </li>
+                    <li className="sidebar-items">
+                        <div className="mr-[8px]"><i class="fa-solid fa-house"></i></div>
+                        <span>Hồ sơ tài liệu</span>
+                    </li>
+                    <li className="sidebar-items">
+                        <div className="mr-[8px]"><i class="fa-solid fa-house"></i></div>
+                        <span>Hồ sơ tài liệu</span>
+                    </li>
+                    <li className="sidebar-items">
+                        <div className="mr-[8px]"><i class="fa-solid fa-house"></i></div>
+                        <span>Hồ sơ tài liệu</span>
+                    </li>
+                    <li className="sidebar-items">
+                        <div className="mr-[8px]"><i class="fa-solid fa-house"></i></div>
+                        <span>Hồ sơ tài liệu</span>
+                    </li>
+                    <li className="sidebar-items">
+                        <div className="mr-[8px]"><i class="fa-solid fa-house"></i></div>
+                        <span>Hồ sơ tài liệu</span>
+                    </li>
+                </ul>
+            </div>
         </>
     )
 }
