5d601ce73a1fc10568cf86181489abc63997114d
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-03-22 22:04:23 +0700

    add form

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,10 @@
 const Home = () =>{
     return (
         <>
-        
+            <button className="h-[40px] ml-[8px] bg-slate-600 rounded-[5px] mt-[5px] px-[8px] text-white font-medium">
+                Thêm hồ sơ mới
+            </button>
+            
         </>
     )
 }
