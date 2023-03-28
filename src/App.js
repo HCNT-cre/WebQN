96454402bbac0fbe89b0af12849fc3af33f91d74
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-03-28 14:18:14 +0700

    add routes

--- src/App.js
+++ src/App.js
@@ -2,20 +2,35 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from './pages/Home'
 import Login from './pages/Login'
-import AddFile from "./pages/AddFile";
+import AddFile from './pages/Files/AddFile'
+import DigitizingFile from './pages/Files/DigitizingFile'
+import DueFile from './pages/Files/DueFile'
 const App = () => {
+    const routes =
+        [
+            { path: "/", element: <Home /> },
+            { path: "/ho-so/tao-ho-so-dien-tu", element: <AddFile /> },
+            { path: "/ho-so/so-hoa-ho-so-tai-lieu", element: <DigitizingFile /> },
+            { path: "/ho-so/ho-so-den-han-nop-luu", element: <DueFile /> },
+        ]
+
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/Login" element={<Login />} />
-                        <Route path="/" element={
-                    <Layout>
-                        <Home />
-                    </Layout>} />
-                <Route path="/them-ho-so" element={
-                    <Layout>
-                        <AddFile />
-                    </Layout>} />
+
+                {routes.map((route, index) => {
+                    return (
+                        <Route path={route.path} element={
+                            <Layout>
+                                {route.element}
+                            </Layout>
+                        } />
+                    )
+                }
+                )}
+
+
             </Routes>
         </BrowserRouter>
     );
