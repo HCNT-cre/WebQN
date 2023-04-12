e20b25cb5431a792e60a7fa3b7891476e69775fa
nxhieu <nguyenxuanhieuktb10@gmail.com>
2023-04-13 01:45:15 +0700

    add storage global

---
+++ src/storage/Storage.js
@@ -0,0 +1,7 @@
+export const reloadPage = () => {
+    document.location.reload()
+}
+
+export const STATE = [
+    "", "Mở", "Đóng", "Nộp lưu cơ quan", "Lưu trữ cơ quan", "Nộp lưu lịch sử", "Lưu trữ lịch sử"
+]
