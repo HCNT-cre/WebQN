ea396b38d7fc6df7bf78a4188bcdd3944f7e61b6
nxhieu <nguyenxuanhieuktb10@gmail.com>
2023-04-13 02:09:39 +0700

    remove reload function

--- src/storage/Storage.js
+++ src/storage/Storage.js
@@ -1,7 +1,3 @@
-export const reloadPage = () => {
-    document.location.reload()
-}
-
 export const STATE = [
     "", "Mở", "Đóng", "Nộp lưu cơ quan", "Lưu trữ cơ quan", "Nộp lưu lịch sử", "Lưu trữ lịch sử"
 ]
