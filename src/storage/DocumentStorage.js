623351ca653b68199abab245a859681c0589b765
nxhieu <nguyenxuanhieuktb10@gmail.com>
2023-04-16 22:36:44 +0700

    add fields

--- src/storage/DocumentStorage.js
+++ src/storage/DocumentStorage.js
@@ -1,9 +1,18 @@
+import { LANGUAGE, FORMAT, RIGHTS } from "./FileStorage"
+
 export const FORM_FIELDS = [
     { key: "doc_ordinal", title: "Số thứ tự", require: true, type: "number" },
     { key: "doc_code", title: "Mã định danh văn bản", require: false, type: "text" },
     { key: "identifier", title: "Mã cơ quan lưu trữ lịch sử", require: false, type: "text" },
-    { key: "issued_date", title: "Ngày, tháng, năm văn bản", require: false, type: "text" },
+    { key: "issued_date", title: "Ngày, tháng, năm văn bản", require: true, type: "date" },
     { key: "autograph", title: "Bút tích", require: false, type: "text" },
+
+    { key: "mode", title: "Chế độ sử dụng", require: true, type: "select", options: RIGHTS},
+    { key: "language", title: "Ngôn ngữ", require: true, type: "select", options: LANGUAGE},
+    // { key: "confidence_level", title: "Mức độ tin cậy", require: true, type: "select", options: },
+    { key: "format", title: "Tình trạng vật lý", require: true, type: "select", options: FORMAT},
+
+
     { key: "code_number", title: "Số của văn bản", require: false, type: "text" },
     { key: "organ_id", title: "Mã phông/công trình/sưu tập lưu trữ", require: false, type: "text" },
     { key: "file_catalog", title: "Mục lục số hoặc năm hình thành hồ sơ", require: false, type: "number" },
@@ -12,13 +21,8 @@ export const FORM_FIELDS = [
     { key: "code_notation", title: "Ký hiệu của văn bản", require: false, type: "text" },
     { key: "organ_name", title: "Tên cơ quan, tổ chức ban hành văn bản", require: false, type: "text" },
     { key: "subject", title: "Trích yếu nội dung", require: false, type: "text" },
-    { key: "language", title: "Ngôn ngữ", require: false, type: "text" },
     { key: "page_amount", title: "Số lượng trang của văn bản", require: false, type: "number" },
     { key: "description", title: "Ghi chú", require: false, type: "text" },
     { key: "infor_sign", title: "Ký hiệu thông tin", require: false, type: "text" },
     { key: "keyword", title: "Từ khóa", require: false, type: "text" },
-    { key: "mode", title: "Chế độ sử dụng", require: false, type: "text" },
-    { key: "confidence_level", title: "Mức độ tin cậy", require: false, type: "text" },
-    { key: "format", title: "Tình trạng vật lý", require: false, type: "text" },
-
 ]
