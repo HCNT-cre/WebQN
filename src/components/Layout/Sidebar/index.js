da4f4d4b73fe23079c9b7f6665624b74c1cb658d
nxhieu <nguyenxuanhieuktb10@gmail.com>
2023-04-18 17:54:06 +0700

    fix siderbar

--- src/components/Layout/Sidebar/index.js
+++ src/components/Layout/Sidebar/index.js
@@ -4,9 +4,33 @@ import QuocHuy from '../../../assets/img/QuocHuy.png'
 import Expand from "react-expand-animated"
 import { TABS_SIDEBAR } from "../../../storage/Storage"
 
+const ChildTabs = ({tab, setCurrentTab, currentTab}) => {
+    const transitions = ["height"];
+    const children = []
+
+
+    for (let i = 0; i < tab.numChildTabs; i++) {
+        const childTab = tab.childTabs[i]
+        if (childTab.numChildTabs > 0) {
+            children.push(<ChildTabs tab={childTab} />)
+        }
+        else
+            children.push(
+                <Link onClick={() => { setCurrentTab(prev => childTab.key) }} to={childTab.to} key={childTab.key} className={`flex items-center text-[#b7b7b7] hover:text-white py-[8px] px-[16px] text-[14px] ${childTab.key === currentTab ? "sidebar-items--active" : ""}`} >
+                    {childTab.title}
+                </Link>)
+    }
+    return (
+        <Expand open={tab.isExpand} duration={200} transitions={transitions}>
+            {children}
+        </Expand>
+    )
+}
+
+
+
 const SideBar = ({ sideBarWidth }) => {
     const locaiton = useLocation()
-    const transitions = ["height"];
     const [sidebarTabs, setSidebarTabs] = useState(TABS_SIDEBAR)
 
     const toggleExpand = (idx) => {
@@ -17,48 +41,41 @@ const SideBar = ({ sideBarWidth }) => {
         })
         setSidebarTabs(cur)
     }
-    const [currentTab, setCurrentTab] = useState(sidebarTabs.findIndex((tab) => {
-        return tab.to === locaiton.pathname.toLocaleLowerCase();
-    }))
 
+
+    const [currentTab, setCurrentTab] = useState(locaiton.pathname.toLocaleLowerCase())
+    console.log(currentTab)
     return (
         <>
-            <div className={`overflow-auto overflow-x-hidden transition-all ${sideBarWidth === 250 ? "w-[250px]" : "w-[80px]"} fixed left-0 top-0 shadow h-full bg-[#302f2ff4] pb-[20px]`}>
+            <div className={`overflow-auto overflow-x-hidden transition-all duration-300 ${sideBarWidth === 250 ? "w-[250px]" : "w-[80px]"} fixed left-0 top-0 shadow h-full bg-[#302f2ff4] pb-[20px]`}>
+
                 <Link to='/' className={`${sideBarWidth === 250 ? 'mx-[16px]' : 'mx-[8px]'} border-[#b7b7b7] flex justify-center my-[16px] pb-[32px] border-b-[2px] border-solid `}>
                     <img className="w-[70px]" alt="Quoc Huy" src={QuocHuy} />
                 </Link>
 
                 {sidebarTabs.map((tab, idx) => {
                     return (
-                        <Link to={tab.to} onClick={() => { setCurrentTab(idx) }} key={idx} className={`
+                        <Link to={tab.to} onClick={() => { setCurrentTab(tab.key) }} key={tab.key} className={`
                         block ${sideBarWidth === 250 ? 'sidebar-items--large ' : 'sidebar-items--small '} 
                         `}>
-                            <div className={`${sideBarWidth !== 250 ? "justify-center" : ""} hover:bg-[#aaaaaa25] border-[1.5px] border-transparent hover:border-cyan-400 rounded-[8px] flex relative ${idx === currentTab ? "sidebar-items--active" : ""}`} onClick={() => toggleExpand(idx)}>
+
+                            <div className={`${sideBarWidth !== 250 ? "justify-center" : ""} hover:bg-[#aaaaaa25] border-[1.5px] border-transparent hover:border-cyan-400 rounded-[8px] flex relative ${tab.key === currentTab ? "sidebar-items--active" : ""}`} onClick={() => toggleExpand(idx)}>
                                 <div className="mx-[10px] w-[16px]">
                                     <span dangerouslySetInnerHTML={{ __html: tab.icon }} />
                                 </div>
+
                                 {sideBarWidth === 250 && <p>{tab.title}</p>}
-                                {tab.numChildTabs > 1 && sideBarWidth === 250 &&
+                                {tab.numChildTabs > 0 && sideBarWidth === 250 &&
                                     <span className="absolute right-[8px] top-0">
                                         {tab.isExpand === false ? <i className="fa-solid fa-chevron-down"></i> : <i className="fa-solid fa-chevron-up"></i>}
                                     </span>
 
                                 }
+
                             </div>
-                            {sideBarWidth === 250 &&
-                                <Expand open={tab.isExpand} duration={200} transitions={transitions}>
-                                    {tab.numChildTabs > 1 &&
-                                        <div className="flex ml-[30px] flex-col">
-                                            {tab.childTabs.map((child, index) => {
-                                                return (
-                                                    <Link key={index} className="text-[14px] px-[8px] hover:bg-[#aaaaaa25] border-[1.5px] border-transparent hover:border-cyan-400 rounded-[8px] block mt-[8px]" to={child.to} >
-                                                        {sideBarWidth === 250 && <span>{child.title}</span>}
-                                                    </Link>
-                                                )
-                                            })}
-                                        </div>
-                                    }
-                                </Expand>
+
+                            {sideBarWidth === 250 && tab.numChildTabs > 0 &&
+                                <ChildTabs tab={tab} setCurrentTab={setCurrentTab} currentTab={currentTab} />
                             }
                         </Link>
                     )
--- src/storage/Storage.js
+++ src/storage/Storage.js
@@ -3,40 +3,54 @@ export const STATE = [
 ]
 
 export const TABS_SIDEBAR = [
-    { isExpand: false, icon: `<i class="fa-solid fa-house"></i>`, title: "Trang chủ", to: "/", numChildTabs: 1 },
+    { isExpand: false, icon: `<i class="fa-solid fa-house"></i>`, title: "Trang chủ", to: "/", numChildTabs: 0, key: "/" },
     {
-        isExpand: false, icon: '<i class="fa-regular fa-file"></i>',
+        key: "/ho-so/", isExpand: false, icon: '<i class="fa-regular fa-file"></i>',
         title: "Hồ sơ tài liệu", numChildTabs: 3, childTabs: [
-            { title: "Danh sách hồ sơ", to: "/ho-so/tao-ho-so-dien-tu" },
-            { title: "Số hóa hồ sơ tài liệu", to: "/ho-so/so-hoa-ho-so-tai-lieu" },
-            { title: "HS đến hạn nộp lưu", to: "/ho-so/ho-so-den-han-nop-luu" }]
+            { title: "Danh sách hồ sơ", to: "/ho-so/tao-ho-so-dien-tu", key: "/ho-so/tao-ho-so-dien-tu" },
+            { title: "Số hóa hồ sơ tài liệu", to: "/ho-so/so-hoa-ho-so-tai-lieu", key: "/ho-so/so-hoa-ho-so-tai-lieu" },
+            { title: "HS đến hạn nộp lưu", to: "/ho-so/ho-so-den-han-nop-luu", key: "/ho-so/ho-so-den-han-nop-luu" }]
     },
     {
-        isExpand: false, icon: '<i class="fa-regular fa-building"></i>',
+        key: "/luu-tru-co-quan/", isExpand: false, icon: '<i class="fa-regular fa-building"></i>',
         title: "Lưu trữ cơ quan", numChildTabs: 3, childTabs: [
-            { title: "Hồ sơ tài liệu giao nộp", to: "/luu-tru-co-quan/ho-so-tai-lieu-giao-nop" },
-            { title: "Kho lưu trữ cơ quan", to: "/luu-tru-co-quan/kho-luu-tru-co-quan" },
-            { title: "HS đến hạn nộp lưu LS", to: "/luu-tru-co-quan/ho-so-den-han-nop-luu-lich-su" }
+            { title: "Hồ sơ tài liệu giao nộp", to: "/luu-tru-co-quan/ho-so-tai-lieu-giao-nop", key: "/luu-tru-co-quan/ho-so-tai-lieu-giao-nop" },
+            { title: "Kho lưu trữ cơ quan", to: "/luu-tru-co-quan/kho-luu-tru-co-quan", key: "/luu-tru-co-quan/kho-luu-tru-co-quan" },
+            { title: "HS đến hạn nộp lưu LS", to: "/luu-tru-co-quan/ho-so-den-han-nop-luu-lich-su", key: "/luu-tru-co-quan/ho-so-den-han-nop-luu-lich-su" }
         ]
     },
     {
-        isExpand: false, icon: '<i class="fa-solid fa-arrow-rotate-right"></i>',
+        key: "/luu-tru-lich-su/", isExpand: false, icon: '<i class="fa-solid fa-arrow-rotate-right"></i>',
         title: "Lưu trữ lịch sử", numChildTabs: 2, childTabs: [
-            { title: "Hồ sơ tài liệu giao nộp", to: "/luu-tru-lich-su/ho-so-tai-lieu-giao-nop" },
-            { title: "Kho lưu trữ lịch sử", to: "/luu-tru-lich-su/kho-luu-tru-lich-su" },
+            { title: "Hồ sơ tài liệu giao nộp", to: "/luu-tru-lich-su/ho-so-tai-lieu-giao-nop", key: "/luu-tru-lich-su/ho-so-tai-lieu-giao-nop" },
+            { title: "Kho lưu trữ lịch sử", to: "/luu-tru-lich-su/kho-luu-tru-lich-su", key: "/luu-tru-lich-su/kho-luu-tru-lich-su" },
         ]
     },
     {
-        isExpand: false, icon: '<i class="fa-solid fa-magnifying-glass"></i>', title: "Tra cứu và tìm kiếm", numChildTabs: 1, to: "/tra-cuu-va-tim-kiem"
+        key: "/khai-bao-danh-muc/", isExpand: false, icon: '<i class="fa-solid fa-list"></i>',
+        title: "Khai báo danh mục", numChildTabs: 3, childTabs: [
+            { title: "Danh mục hồ sơ", to: "/khai-bao-danh-muc/danh-muc-ho-so", key: "/khai-bao-danh-muc/danh-muc-ho-so" },
+            { title: "Danh mục kho lưu trữ", to: "/khai-bao-danh-muc/danh-muc-kho-luu-tru", key: "/khai-bao-danh-muc/danh-muc-kho-luu-tru", isExpand: false, numChildTabs: 3, childTabs: [
+                { title: "Kho", to: "/khai-bao-danh-muc/danh-muc-kho-luu-tru/kho", key: "/khai-bao-danh-muc/danh-muc-kho-luu-tru/kho" },
+                { title: "Kệ", to: "/khai-bao-danh-muc/danh-muc-kho-luu-tru/ke", key: "/khai-bao-danh-muc/danh-muc-kho-luu-tru/ke" },
+                { title: "Tầng", to: "/khai-bao-danh-muc/danh-muc-kho-luu-tru/tang", key: "/khai-bao-danh-muc/danh-muc-kho-luu-tru/tang" },
+
+
+            ]},
+            { title: "Danh mục cơ quan", to: "/khai-bao-danh-muc/danh-muc-co-quan", key: "/khai-bao-danh-muc/danh-muc-co-quan" },
+        ]
     },
     {
-        isExpand: false, icon: '<i class="fa-regular fa-newspaper"></i>', title: "Báo cáo và thống kê", numChildTabs: 1, to: "/bao-cao-va-thong-ke"
+        isExpand: false, icon: '<i class="fa-solid fa-magnifying-glass"></i>', title: "Tra cứu và tìm kiếm", numChildTabs: 0, to: "/tra-cuu-va-tim-kiem", key: "/tra-cuu-va-tim-kiem"
     },
     {
-        isExpand: false, icon: '<i class="fa-solid fa-people-roof"></i>',
+        isExpand: false, icon: '<i class="fa-regular fa-newspaper"></i>', title: "Báo cáo và thống kê", numChildTabs: 0, to: "/bao-cao-va-thong-ke", key: "/bao-cao-va-thong-ke"
+    },
+    {
+        key: "/quan-ly-he-thong/", isExpand: false, icon: '<i class="fa-solid fa-people-roof"></i>',
         title: "Quản lý hệ thống", numChildTabs: 2, childTabs: [
-            { title: "Người dùng", to: "/quan-ly-he-thong/nguoi-dung" },
-            { title: "Phân quyền hệ thống", to: "/quan-ly-he-thong/phan-quyen-he-thong" },
+            { title: "Người dùng", to: "/quan-ly-he-thong/nguoi-dung", key: "/quan-ly-he-thong/nguoi-dung" },
+            { title: "Phân quyền hệ thống", to: "/quan-ly-he-thong/phan-quyen-he-thong", key: "/quan-ly-he-thong/phan-quyen-he-thong" },
         ]
     },
 ]
