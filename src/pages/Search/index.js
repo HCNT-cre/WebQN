667767871b4d343883d35fb348de409bf061637b
nxhieu <nguyenxuanhieuktb10@gmail.com>
2023-04-22 16:16:48 +0700

    add watch doc when click on searched file

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,23 +1,25 @@
-import React, { useRef, useState } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import { Spin } from "antd";
 import { FaSearch } from "react-icons/fa";
 import "./searchbar.css";
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
 import { OpenFile } from "../../actions/formFile";
+import Doc from "./Doc";
 
 const API_SEARCH = process.env.REACT_APP_API_SEARCH;
 const API_GOV_FILE_GET = process.env.REACT_APP_API_GOV_FILE_GET
 
 const Searchbar = () => {
+  const [stateDoc, setStateDoc] = useState(false)
+  const [idDOc, setIdDoc] = useState(null)
+  const [govFileId, setGovFileId] = useState(null)
   const refInput = useRef();
   const [searching, setSearching] = useState(false);
   const [completeGetAPI, setCompleteGetAPI] = useState(false);
   const [searchedFile, setSearchedFile] = useState([]);
   const [textSearch, setTextSearch] = useState("");
-  const dispatch = useDispatch();
   const userPermissionId = useSelector(state => state.user.permission_id)
-
   const getFileName = async (items) => {
     const searchedFileWithFileName = []
     for (const item of items) {
@@ -31,10 +33,8 @@ const Searchbar = () => {
     setSearchedFile(searchedFileWithFileName)
   }
 
-  console.log(searchedFile)
   const handleSearch = (e) => {
     const query = refInput.current.value;
-    setTextSearch(query)
 
     const fetchData = () => {
       setSearching(true);
@@ -42,6 +42,8 @@ const Searchbar = () => {
         const response = await axios.post(API_SEARCH, {
           query: query,
         });
+        setTextSearch(query)
+
         setCompleteGetAPI(true);
         setSearching(false);
         setTimeout(() => {
@@ -56,62 +58,69 @@ const Searchbar = () => {
 
   };
 
-  const viewFile = (id) => {
-    dispatch(OpenFile("watch_file", id));
+  const viewFile = (idFile, idDoc) => {
+    setIdDoc(idDoc)
+    setGovFileId(idFile)
+    setStateDoc(true)
   };
 
   return (
-    <div className="bg-white h-[80vh] relative">
-      <div
-        className={`w-full flex justify-center flex-col items-center absolute transition-all top-[150px] duration-300 ${completeGetAPI === true ? "search-bar-after-search" : ""
-          }`}
-      >
-        <h1 className="text-[25px] font-bold pb-[20px] duration-300 transition-all">
-          Tìm kiếm văn bản theo nội dung
-        </h1>
-        <div className="input-wrapper mb-[20px] duration-300 transition-all">
-          {searching ? (
-            <Spin />
-          ) : (
-            <FaSearch
-              className="w-[21px] h-[25.2px] "
-              id="search-icon"
-              onClick={handleSearch}
+    <Fragment>
+      <div className="bg-white h-[80vh] relative">
+        <div
+          className={`w-full flex justify-center flex-col items-center absolute transition-all top-[150px] duration-300 ${completeGetAPI === true ? "search-bar-after-search" : ""
+            }`}
+        >
+          <h1 className="text-[25px] font-bold pb-[20px] duration-300 transition-all">
+            Tìm kiếm văn bản theo nội dung
+          </h1>
+          <div className="input-wrapper mb-[20px] duration-300 transition-all">
+            {searching ? (
+              <Spin />
+            ) : (
+              <FaSearch
+                className="w-[21px] h-[25.2px] "
+                id="search-icon"
+                onClick={handleSearch}
+              />
+            )}
+            <input
+              ref={refInput}
+              placeholder="Nhập nội dung cần tìm kiếm..."
+              onKeyDown={handleSearch}
+              disabled={searching}
             />
-          )}
-          <input
-            ref={refInput}
-            placeholder="Nhập nội dung cần tìm kiếm..."
-            onKeyDown={handleSearch}
-            disabled={searching}
-          />
+          </div>
+        </div>
+        <div>
+          <div className="pt-[120px] px-[10%]">
+            {searchedFile.length > 0 &&
+              searchedFile.map((file, index) => {
+                return (
+                  <div className="border-[2px] border-solid p-[8px] rounded-[5px]  mb-[16px]">
+                    <div className="flex justify-left items-center text-[20px]">
+                      <span
+                        onClick={() => viewFile(file.gov_file_id, file.doc_id)}
+                        className="cursor-pointer text-[rgba(0,0,0,.45)]"
+                      >
+                        {file.file_name}
+                      </span>
+                      &nbsp;/ &nbsp;
+                      <span className="cursor-pointer" onClick={() => { viewFile(file.gov_file_id, file.doc_id) }}>{file.doc_name}</span>
+                    </div>
+                    <div className="font-bold">
+                      {textSearch}
+                    </div>
+                  </div>
+                );
+              })}
+
+          </div>
         </div>
       </div>
-      <div>
-        <div className="pt-[120px] px-[10%]">
-          {searchedFile.length > 0 &&
-            searchedFile.map((file, index) => {
-              return (
-                <div className="border-[2px] border-solid p-[8px] rounded-[5px]  mb-[16px]">
-                  <div className="flex justify-left items-center text-[20px]">
-                    <span
-                      onClick={() => viewFile(file.gov_file_id)}
-                      className="cursor-pointer text-[rgba(0,0,0,.45)]"
-                    >
-                      {file.file_name}
-                    </span>
-                    &nbsp;/ &nbsp;
-                    <span className="cursor-pointer" onClick={() => { }}>{file.doc_name}</span>
-                  </div>
-                  <div className="font-bold">
-                    {textSearch}
-                  </div>
-                </div>
-              );
-            })}
-        </div>
-      </div>
-    </div>
+      <Doc stateFixDoc={stateDoc} setStateFixDoc={setStateDoc} id={idDOc} govFileId={govFileId} />
+    </Fragment>
+
   );
 };
 
