350b297ef63cccc856621482597ee89db1f980c4
HCNT-cre <cuonghung2k3@gmail.com>
2023-04-24 09:12:28 +0700

    fix mt header and ui change role and logout

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -16,7 +16,7 @@ const Header = ({ sideBarWidth, setSideBarWidth }) => {
 
     return (
         <>
-            <div className={`z-50 flex justify-between px-[16px] transition-all ${sideBarWidth === 250 ? "ml-[260px] w-[calc(99%-255px)]" : "ml-[90px] w-[calc(99%-85px)]"} h-[75px]  border-[2px] border-blue-700 rounded-md bg-blue-300 top-0`}>
+            <div className={`z-50 flex justify-between px-[16px] transition-all ${sideBarWidth === 250 ? "ml-[260px] w-[calc(99%-255px)]" : "ml-[90px] w-[calc(99%-85px)]"} h-[75px]  mt-[8px] border-[2px] border-blue-700 rounded-md bg-blue-300 top-0`}>
                 <div className="flex justify-between items-center">
                     <button className="mr-[20px] text-[25px] font-bold text-black cursor-pointer toggle-side-bar-button" onClick={() => {
                         sideBarWidth === 250 ? setSideBarWidth(50) : setSideBarWidth(250)
@@ -36,7 +36,7 @@ const Header = ({ sideBarWidth, setSideBarWidth }) => {
                     {
                         stateBoxUserRole &&
                         <div className="absolute top-[45px] w-[calc(100%-36px)] ">
-                            <div className="text-center w-full  bg-white rounded-[8px] p-[8px] mt-[8px] text-[12px] cursor-pointer border-solid border-[1px] border-[#ccc] shadow-sm ml-[36px]">
+                            <div className="text-center w-full  bg-white font-medium rounded-[8px] p-[8px] mt-[8px] text-[12px] cursor-pointer border-solid border-[1px]  hover:bg-blue-700 hover:text-white border-[#ccc] shadow-sm ml-[36px]">
                                 {
                                     USER_ROLES.filter(role => role !== userRole).map((role, index) => {
                                         return (
@@ -56,7 +56,7 @@ const Header = ({ sideBarWidth, setSideBarWidth }) => {
                                 }
                             </div>
 
-                            <div onClick={handleLogOut} className="text-center w-full  bg-white rounded-[8px] p-[8px] text-[12px] cursor-pointer border-solid border-[1px] border-[#ccc] shadow-sm ml-[36px]">Đăng xuất</div>
+                            <div onClick={handleLogOut} className="text-center w-full font-medium text-black bg-white rounded-[8px] p-[8px] text-[12px] cursor-pointer border-solid border-[1px] border-[#ccc] shadow-sm ml-[36px] hover:bg-blue-700 hover:text-white">Đăng xuất</div>
                         </div>
 
                     }
