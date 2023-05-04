f030bab9849ff33ae702d0242a738ef96e40cf8c
nxhieu <nguyenxuanhieuktb10@gmail.com>
2023-05-04 11:29:43 +0700

    change some pdf file

--- src/storage/Eoffice.js
+++ src/storage/Eoffice.js
@@ -1,28 +1,3 @@
-export const EofficeFile = [
-    {
-        gov_file_id_of_doc: 29,
-        gov_file_code: "001.03.34.H48.2012.01/MTSL",
-        file_name: "Về Mạng truyền số liệu chuyên dùng phục vụ các cơ quan Đảng, Nhà nước",
-
-        doc: [
-            {
-                id: 1,
-                file_notation: "81/NDTD", subject: "Công tác chuẩn bị thi cuối kì 2 năm học 2022-2023", doc_name: "04-ttg.signed.pdf", link: "http://178.128.123.34/api/media/upload_documents/29/04-ttg.signed.pdf"
-            },
-            { id: 2, file_notation: "01/MTSL", subject: "Quy định về thí điểm phân cấp thẩm quyền phê duyệt trình tự thủ tục quy hoạch xây dựng khu chức năng", doc_name: "07-ttg.signed.pdf", link: "http://178.128.123.34/api/media/upload_documents/29/07-ttg.signed.pdf" },
-        ]
-    },
-    {
-        gov_file_id_of_doc: 39,
-        gov_file_code: "001.03.34.H48.2011.02/THPT-QN",
-        file_name: "Hồ sơ thi THPT 2011",
-        doc: [
-            { id: 3, file_notation: "VIO/20/CMD", subject: "Tuyên truyền tổ chức lấy ý kiến Nhân dân đối với dự thảo Luật Đất đai (sửa đổi)", doc_name: "TTTT.pdf", link: "http://178.128.123.34/api/media/upload_documents/39/TTTT.pdf" },
-            { id: 4, file_notation: "35/SSH/32", subject: "Nghị định về công tác văn thư", doc_name: "30.signed.pdf", link: "http://178.128.123.34/api/media/upload_documents/39/30.signed.pdf" },
-        ]
-    }
-]
-
 export const INCOMING_DOC = [
     {
         id: 1, file_notation: "97/STTTT-TTBCXB", subject: "V/v tuyên truyền tổ chức lấy ý kiến Nhân dân đối với dự thảo Luật Đất đai (sửa đổi).", doc_name: "TTTT.pdf", link: "http://178.128.123.34/api/media/upload_documents/39/TTTT.pdf"
