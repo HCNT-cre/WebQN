58464d391a58aa9a493a13eeb4f1d0d29b839105
Bo <nguyendqduy@gmail.com>
2023-06-25 13:24:17 +0700

    add filter

--- src/pages/ThuThapVaNopLuu/NopLuuCoQuan.js
+++ src/pages/ThuThapVaNopLuu/NopLuuCoQuan.js
@@ -1,16 +1,37 @@
 import BasePage from "../BasePage";
 
 const NopLuuCoQuan = () => {
-    const parent = [
-        { title: "Thu thập và nộp lưu", link: "/thu-thap-va-nop-luu/tao-ke-hoach-thu-thap" },
-    ]
+  const parent = [
+    {
+      title: "Thu thập và nộp lưu",
+      link: "/thu-thap-va-nop-luu/tao-ke-hoach-thu-thap",
+    },
+  ];
 
-    const current = {
-        link: "/thu-thap-va-nop-luu/nop-luu-co-quan",
-        title: "Nộp lưu cơ quan"
+  const current = {
+    link: "/thu-thap-va-nop-luu/nop-luu-co-quan",
+    title: "Nộp lưu cơ quan",
+  };
+  const filter = (files) => {
+    const newFiles = [];
+    console.log(files);
+    for (const file of files) {
+      console.log(file.state.props.children);
+      if (file.state.props.children !== "Nộp lưu cơ quan") continue;
+      newFiles.push(file);
     }
 
-    return <BasePage parent={parent} current={current} addNewFile={true} />
-}
+    return newFiles;
+  };
+
+  return (
+    <BasePage
+      parent={parent}
+      current={current}
+      addNewFile={true}
+      filter={filter}
+    />
+  );
+};
 
 export default NopLuuCoQuan;
