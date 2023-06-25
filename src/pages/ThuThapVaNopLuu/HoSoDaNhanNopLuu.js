92b7be7a724376379563d55e70de92bb5bd1fc60
Bo <nguyendqduy@gmail.com>
2023-06-25 14:11:02 +0700

    add filter

--- src/pages/ThuThapVaNopLuu/HoSoDaNhanNopLuu.js
+++ src/pages/ThuThapVaNopLuu/HoSoDaNhanNopLuu.js
@@ -1,16 +1,35 @@
 import BasePage from "../BasePage";
 
 const HoSoDaNhanNopLuu = () => {
-    const parent = [
-        { title: "Thu thập và nộp lưu", link: "/thu-thap-va-nop-luu/tao-ke-hoach-thu-thap" },
-    ]
+  const parent = [
+    {
+      title: "Thu thập và nộp lưu",
+      link: "/thu-thap-va-nop-luu/tao-ke-hoach-thu-thap",
+    },
+  ];
 
-    const current = {
-        link: "/thu-thap-va-nop-luu/ho-so-da-nhan-nop-luu",
-        title: "Hồ sơ đã nhận nộp lưu"
+  const current = {
+    link: "/thu-thap-va-nop-luu/ho-so-da-nhan-nop-luu",
+    title: "Hồ sơ đã nhận nộp lưu",
+  };
+
+  const filter = (files) => {
+    const newFiles = [];
+    for (const file of files) {
+      if (file.state.props.children !== "Lưu trữ cơ quan") continue;
+      newFiles.push(file);
     }
+    return newFiles;
+  };
 
-    return <BasePage parent={parent} current={current} addNewFile={true} />
-}
+  return (
+    <BasePage
+      parent={parent}
+      current={current}
+      addNewFile={true}
+      filter={filter}
+    />
+  );
+};
 
 export default HoSoDaNhanNopLuu;
