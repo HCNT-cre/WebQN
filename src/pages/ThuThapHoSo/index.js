09e366a2187772de5c59d441fd7081fce82c5b57
Bo <nguyendqduy@gmail.com>
2023-06-25 14:11:17 +0700

    format

--- src/pages/ThuThapHoSo/index.js
+++ src/pages/ThuThapHoSo/index.js
@@ -1,16 +1,14 @@
 import BasePage from "../BasePage";
 
-const ThuThapHoSo = ()=>{
-    const parent = [
-        
-    ]
+const ThuThapHoSo = () => {
+  const parent = [];
 
-    const current = {
-        link: "/thu-thap-ho-so",
-        title: "Thu thập hồ sơ"
-    }
+  const current = {
+    link: "/thu-thap-ho-so",
+    title: "Thu thập hồ sơ",
+  };
 
-    return <BasePage parent={parent} current={current} addNewFile={true}/>
-}
+  return <BasePage parent={parent} current={current} addNewFile={true} />;
+};
 
 export default ThuThapHoSo;
