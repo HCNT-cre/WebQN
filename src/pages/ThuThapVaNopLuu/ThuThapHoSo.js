084ded6426d476f9282e16dd9fe980236b89b57b
nxhieu3102 <nguyenxuanhieuktb10@gmail.com>
2023-08-01 00:22:57 +0700

    re format

--- src/pages/ThuThapVaNopLuu/ThuThapHoSo.js
+++ src/pages/ThuThapVaNopLuu/ThuThapHoSo.js
@@ -1,41 +1,40 @@
-import { CleaningServices } from "@mui/icons-material";
 import BasePage from "../BasePage";
 
 const ThuThapHoSo = () => {
-  const parent = [
-    {
-      title: "Thu thập và nộp lưu",
-      link: "/thu-thap-va-nop-luu/tao-ke-hoach-thu-thap",
-    },
-  ];
+    const parent = [
+        {
+            title: "Thu thập và nộp lưu",
+            link: "/thu-thap-va-nop-luu/tao-ke-hoach-thu-thap",
+        },
+    ];
 
-  const current = {
-    link: "/thu-thap-va-nop-luu/thu-thap-ho-so",
-    title: "Thu thập hồ sơ",
-  };
+    const current = {
+        link: "/thu-thap-va-nop-luu/thu-thap-ho-so",
+        title: "Thu thập hồ sơ",
+    };
 
-  const filter = (files) => {
-    const newFiles = [];
-    for (const file of files) {
-      if (
-        file.state.props.children !== "Đóng" &&
-        file.state.props.children !== "Mở"
-      )
-        continue;
-      newFiles.push(file);
-    }
+    const filter = (files) => {
+        const newFiles = [];
+        for (const file of files) {
+            if (
+                file.state.props.children !== "Đóng" &&
+                file.state.props.children !== "Mở"
+            )
+                continue;
+            newFiles.push(file);
+        }
 
-    return newFiles;
-  };
+        return newFiles;
+    };
 
-  return (
-    <BasePage
-      parent={parent}
-      current={current}
-      addNewFile={true}
-      filter={filter}
-    />
-  );
+    return (
+        <BasePage
+            parent={parent}
+            current={current}
+            addNewFile={true}
+            filter={filter}
+        />
+    );
 };
 
 export default ThuThapHoSo;
