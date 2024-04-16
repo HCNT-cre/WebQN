2d6d20a458ae78a041c0f5b0218c9f8dd890f49a
hieu <nguyenxuanhieuktb10@gmail.com>
2024-04-16 21:15:35 +0700

    chore: fix .env file

---
+++ config/env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Copy a .env.${NODE_ENV} file (ex: .env.production) as a .env file in the root folder.
+# if NODE_END is not set, will use 'development' as default
+
+set -e # Exit with nonzero exit code if anything fails
+
+ENV_FILE="${PWD}/config/.env.base"
+TARGET="${PWD}/.env"
+ENV=local
+
+echo "== Start .env copy =="
+
+# Checking if env file exist
+if [ ! -f $ENV_FILE ]; then
+    echo "Expected config file '$ENV_FILE' not found. Stopping script."
+    exit 1
+fi
+
+echo "Copying $ENV_FILE to $TARGET..."
+cp $ENV_FILE $TARGET
+
+if [ "$ENV" == "production" ]; then
+    echo $TARGET
+    sed -i '' "s|VITE_API_STORAGE=http://localhost/api/|VITE_API_STORAGE=https://luutrudientu.quangngai.gov.vn/api/|g" $TARGET
+    sed -i '' "s|VITE_API_EXPORT_EXCEL=http://localhost:5678/excel|VITE_API_EXPORT_EXCEL=https://luutrudientu.quangngai.gov.vn/execl|g" $TARGET
+fi
+
+echo "== .env copy completed =="
